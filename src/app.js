af3b6b4e565ae48f3bf9e8ef993e87813f8f79e3
cnaranjo27 <cnaranjo@fullmotos.cl>
2023-02-16 23:50:15 +0000

    generador de excusas

--- src/app.js
+++ src/app.js
@@ -2,10 +2,28 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
+//cuando el navegador renderice el html, entonces
+//ejecutará lo siguiente
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  console.log("vengo de jupiter?");
+  let who = ["Mi perro", "Mi abuela", "Mi tortuga", "Mi gecko"];
+  let action = ["se comio", "se robo", "trituro", "vomito"];
+  let what = ["mi tarea", "las llaves", "mi libro"];
+  let when = [
+    "antes de clase",
+    "durante el almuerzo",
+    "durante mis vacaciones",
+    "en misa",
+    "despues de mi fiesta"
+  ];
+
+  let a = Math.floor(Math.random() * who.length);
+  let b = Math.floor(Math.random() * action.length);
+  let c = Math.floor(Math.random() * what.length);
+  let d = Math.floor(Math.random() * when.length);
+
+  let resultado = who[a] + " " + action[b] + " " + what[c] + " " + when[d];
+
+  document.getElementById("excusa").innerHTML = resultado;
 };
